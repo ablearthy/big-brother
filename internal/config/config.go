a0f12584bebd8e58ea707401c377f5a97d7dd159
Able Arthy <ablearthy@gmail.com>
2022-04-03 12:32:33 +0500

    Create config reader

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+type DbConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Dbname   string `yaml:"dbname"`
+	Sslmode  string `yaml:"sslmode"`
+}
+
+type ServerConfig struct {
+	Port int `yaml:"port"`
+}
+
+type Config struct {
+	Db     DbConfig     `yaml:"db"`
+	Server ServerConfig `yaml:"web"`
+}
+
+func NewConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	d := yaml.NewDecoder(file)
+
+	if err = d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
