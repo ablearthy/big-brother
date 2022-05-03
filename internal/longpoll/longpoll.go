b304f007008d5cb2bd73a2943ca6db3f0430c7f3
ablearthy <ablearthy@gmail.com>
2022-05-03 17:27:00 +0500

    Make LongPollManager private & publish messages

--- internal/longpoll/longpoll.go
+++ internal/longpoll/longpoll.go
@@ -13,31 +13,33 @@ type StatusRequest struct {
 	bCh chan<- bool
 }
 
-type LongPollManager struct {
+type longPollManager struct {
 	clients          map[AccessToken]*sdklp.LongPoll
 	acceptNewTokenCh chan AccessToken
 	deleteTokenCh    chan AccessToken
 	isAliveCh        chan StatusRequest
+	publisherCh      chan<- publisherData
 }
 
-func Init() *LongPollManager {
-	return &LongPollManager{
+func initLongPollManager(publisher chan<- publisherData) *longPollManager {
+	return &longPollManager{
 		clients:          make(map[AccessToken]*sdklp.LongPoll),
 		acceptNewTokenCh: make(chan AccessToken),
 		deleteTokenCh:    make(chan AccessToken),
 		isAliveCh:        make(chan StatusRequest),
+		publisherCh:      publisher,
 	}
 }
 
-func (lpm *LongPollManager) AddNewToken(token AccessToken) {
+func (lpm *longPollManager) AddNewToken(token AccessToken) {
 	lpm.acceptNewTokenCh <- token
 }
 
-func (lpm *LongPollManager) DeleteToken(token AccessToken) {
+func (lpm *longPollManager) DeleteToken(token AccessToken) {
 	lpm.deleteTokenCh <- token
 }
 
-func (lpm *LongPollManager) TokenIsAlive(token AccessToken) <-chan bool {
+func (lpm *longPollManager) TokenIsAlive(token AccessToken) <-chan bool {
 	ch := make(chan bool)
 
 	lpm.isAliveCh <- StatusRequest{token, ch}
@@ -45,7 +47,7 @@ func (lpm *LongPollManager) TokenIsAlive(token AccessToken) <-chan bool {
 	return ch
 }
 
-func (lpm *LongPollManager) registerNewToken(token AccessToken) {
+func (lpm *longPollManager) registerNewToken(token AccessToken) {
 	if _, ok := lpm.clients[token]; ok {
 		return
 	}
@@ -57,14 +59,17 @@ func (lpm *LongPollManager) registerNewToken(token AccessToken) {
 	lp.Goroutine(true)
 	go func() {
 		lp.FullResponse(func(obj object.LongPollResponse) {
-			// TODO: send messages to broker
+			lpm.publisherCh <- publisherData{
+				at:  token,
+				obj: obj,
+			}
 		})
 		lp.Run()
 	}()
 	lpm.clients[token] = lp
 }
 
-func (lpm *LongPollManager) deleteToken(token AccessToken) {
+func (lpm *longPollManager) deleteToken(token AccessToken) {
 	lp, ok := lpm.clients[token]
 	if !ok {
 		return
@@ -73,12 +78,12 @@ func (lpm *LongPollManager) deleteToken(token AccessToken) {
 	delete(lpm.clients, token)
 }
 
-func (lpm *LongPollManager) sendAliveStatus(s *StatusRequest) {
+func (lpm *longPollManager) sendAliveStatus(s *StatusRequest) {
 	_, ok := lpm.clients[s.at]
 	s.bCh <- ok
 }
 
-func (lpm *LongPollManager) Run() {
+func (lpm *longPollManager) Run() {
 	for {
 		select {
 		case newToken := <-lpm.acceptNewTokenCh:
