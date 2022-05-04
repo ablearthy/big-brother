d703732be7ca0694d0658084490dc3ed52b58e4c
ablearthy <ablearthy@gmail.com>
2022-05-04 16:11:55 +0500

    Join `Settings` group to the application

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"big-brother/internal/server/settings"
 	"big-brother/internal/server/user"
+
 	"github.com/labstack/echo/v4"
 )
 
 func SetRoutes(e *echo.Echo) {
 	g := e.Group("/user")
 	user.SetUserGroup(g)
+
+	s := e.Group("/settings")
+	settings.SetSettingsGroup(s)
 }
