aac0b543cccc750fcb9c552e0aaa1deaae2d7e57
ablearthy <ablearthy@gmail.com>
2022-05-05 20:53:34 +0500

    Start working on `TransferLongPollMessagesService`

---
+++ internal/service/sse/sse.go
@@ -0,0 +1,54 @@
+package sse
+
+import (
+	"big-brother/internal/background"
+	"big-brother/internal/db"
+	"big-brother/internal/longpoll"
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TransferLongPollMessagesService struct {
+}
+
+func (*TransferLongPollMessagesService) Transfer(c echo.Context, userId int) error {
+	queries := db.New(db.GetConn())
+	token, err := queries.GetTokenById(context.Background(), int32(userId))
+
+	if err != nil || !token.Valid {
+		return c.NoContent(http.StatusOK)
+	}
+
+	vt, err := queries.GetVkToken(context.Background(), token.String)
+
+	if err != nil {
+		return c.NoContent(http.StatusOK)
+	}
+
+	lpmw := background.GetLongPollManagerWrapper()
+
+	messageCh := make(chan longpoll.Response)
+
+	lpmw.Subscribe(&longpoll.Subscriber{
+		VkUserId: longpoll.VkUserId(vt.VkUserID),
+		Ch:       messageCh,
+	})
+
+	ctx := c.Request().Context()
+
+	c.Response().Header().Set(echo.HeaderCacheControl, "no-store")
+	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
+	c.Response().WriteHeader(http.StatusOK)
+
+	select {
+	case <-ctx.Done():
+		return c.NoContent(http.StatusOK)
+	case msg := <-messageCh:
+		log.Println("New message: ", msg)
+
+	}
+	return c.NoContent(http.StatusOK)
+}
