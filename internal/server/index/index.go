8087f4035e04dd9374c303f765763c9e06e20ba4
ablearthy <ablearthy@gmail.com>
2022-05-08 15:38:43 +0500

    Add `Index` group

---
+++ internal/server/index/index.go
@@ -0,0 +1,15 @@
+package index
+
+import (
+	"big-brother/internal/controller/index"
+
+	"github.com/labstack/echo/v4"
+)
+
+func SetIndexGroup(e *echo.Echo) {
+	ih := &index.IndexHandler{}
+
+	e.GET("/", ih.Index)
+	e.GET("/login", ih.Login)
+	e.GET("/home", ih.Home)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"big-brother/internal/server/index"
 	"big-brother/internal/server/settings"
 	"big-brother/internal/server/sse"
 	"big-brother/internal/server/user"
@@ -9,6 +10,8 @@ import (
 )
 
 func SetRoutes(e *echo.Echo) {
+	index.SetIndexGroup(e)
+
 	g := e.Group("/user")
 	user.SetUserGroup(g)
 	sse.SetSSEGroup(g)
