244578cf92049fc027fb3edb29ee558cde790b74
ablearthy <ablearthy@gmail.com>
2022-05-08 15:37:27 +0500

    Add `IndexHandler`

---
+++ internal/controller/index/index.go
@@ -0,0 +1,26 @@
+package index
+
+import (
+	"big-brother/internal/auth"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IndexHandler struct{}
+
+func (*IndexHandler) Index(c echo.Context) error {
+	_, err := auth.GetUserId(c)
+	if err != nil {
+		return c.Redirect(http.StatusFound, "/login")
+	}
+	return c.Redirect(http.StatusFound, "/home")
+}
+
+func (*IndexHandler) Login(c echo.Context) error {
+	return c.Render(http.StatusOK, "login", struct{}{})
+}
+
+func (*IndexHandler) Home(c echo.Context) error {
+	return c.Render(http.StatusOK, "home", struct{}{})
+}
