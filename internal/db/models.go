290b10e6ad6caab32986902c88f0d788c6128f29
ablearthy <ablearthy@gmail.com>
2022-05-15 13:41:45 +0500

    Update schema

--- internal/db/models.go
+++ internal/db/models.go
@@ -12,6 +12,25 @@ import (
 	"github.com/jackc/pgtype"
 )
 
+type VkActivity string
+
+const (
+	VkActivityOnline  VkActivity = "online"
+	VkActivityOffline VkActivity = "offline"
+)
+
+func (e *VkActivity) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case []byte:
+		*e = VkActivity(s)
+	case string:
+		*e = VkActivity(s)
+	default:
+		return fmt.Errorf("unsupported scan type for VkActivity: %T", src)
+	}
+	return nil
+}
+
 type VkMessageEventType string
 
 const (
@@ -32,6 +51,30 @@ func (e *VkMessageEventType) Scan(src interface{}) error {
 	return nil
 }
 
+type VkPlatform string
+
+const (
+	VkPlatformMobile  VkPlatform = "mobile"
+	VkPlatformIphone  VkPlatform = "iphone"
+	VkPlatformIpad    VkPlatform = "ipad"
+	VkPlatformAndroid VkPlatform = "android"
+	VkPlatformWphone  VkPlatform = "wphone"
+	VkPlatformWindows VkPlatform = "windows"
+	VkPlatformWeb     VkPlatform = "web"
+)
+
+func (e *VkPlatform) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case []byte:
+		*e = VkPlatform(s)
+	case string:
+		*e = VkPlatform(s)
+	default:
+		return fmt.Errorf("unsupported scan type for VkPlatform: %T", src)
+	}
+	return nil
+}
+
 type InviteCode struct {
 	UserID     int32
 	InviteCode string
@@ -47,6 +90,16 @@ type User struct {
 type UserToken struct {
 	UserID      int32
 	AccessToken sql.NullString
+}
+
+type VkActivityEvent struct {
+	ID              int32
+	VkOwnerID       int32
+	TargetID        int32
+	Activity        VkActivity
+	Platform        VkPlatform
+	KickedByTimeout sql.NullBool
+	CreatedAt       time.Time
 }
 
 type VkMessage struct {
